6733f8403176ead03cd545fa7dc71ec06ea5c022
yihui8500 <yihui8500@163.com>
2025-01-05 07:13:03 +0000

    20250105

---
+++ file1.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+void printMessage(){
+	printf("Hello from printMessage!\n");
+}
---
+++ file2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+extern void printMessage();
+
+int main(){
+	printMessage();
+	return 0;
+}
